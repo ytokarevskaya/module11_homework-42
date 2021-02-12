c1d2200370aa7eb61eb684bd4fb980c46fb93e05
Iuliia Tokarevskaia <iuliia.tokarevskaia@socialbakers.com>
2021-02-12 09:06:12 +0100

    Проверено

--- 11.7_task4.js
+++ 11.7_task4.js
@@ -1,12 +1,13 @@
-
-let startTimer = 8;
-let endTimer = 20;
-
-const intervalId = setInterval(function (a, b) {       
+function printNumbers(startTimer, endTimer) {
+    const intervalId = setInterval(function () {       
         console.log(startTimer);
         startTimer++;
-        if (startTimer === endTimer) {
+        if (startTimer === endTimer + 1) {
             clearInterval(intervalId);
         };
-    }, 1000, startTimer, endTimer+1); 
+    }, 1000); 
+}
 
+printNumbers(5, 10);
+
+// Сам алгоритм таймера написан верно, но по заданию решение нужно было оформить в виде функции, чтобы один и тот же код можно было легко переиспользовать с разными входными данными. Выге исправила
